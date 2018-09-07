248ffbb0e1bb3693dafe84f3717d4054eb362627
George Macon <george.macon@gmail.com>
2018-09-07 19:50:52 -0400

    Add network status

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,36 @@
  * Volume: pactl subscribe
  */
 
+#![recursion_limit = "1024"]
+
 extern crate chrono;
+#[macro_use]
+extern crate error_chain;
+extern crate regex;
+#[macro_use]
+extern crate lazy_static;
 extern crate sensors;
 extern crate systemstat;
 extern crate xcb;
 
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::process::{Command, Stdio};
 use std::sync::{Arc, Condvar, Mutex};
 use std::thread;
 use std::time;
 
+use regex::Regex;
 use sensors::{FeatureType, Sensors, Subfeature, SubfeatureType};
 use systemstat::{Platform, System};
+
+mod errors {
+    error_chain!{}
+}
+
+use errors::*;
 
 const POLL_TIME: time::Duration = time::Duration::from_secs(5);
 
@@ -24,11 +43,12 @@ struct DisplayFields {
     time: String,
     systemstat: String,
     temp: String,
+    net: String,
 }
 
 impl ToString for DisplayFields {
     fn to_string(&self) -> String {
-        format!("{} ⸱ {} ⸱ {}", self.systemstat, self.temp, self.time).to_string()
+        format!("{}{}{}{}", self.net, self.systemstat, self.temp, self.time).to_string()
     }
 }
 
@@ -96,7 +116,8 @@ fn cpu(sys: &System) -> String {
 fn systemstat_thread(conc: Arc<Concurrency>) {
     let sys = System::new();
     loop {
-        let new_stat = format!("{} ⸱ {} ⸱ {}", battery(&sys), ram(&sys), cpu(&sys)).to_string();
+        let new_stat =
+            format!("{} ⸱ {} ⸱ {} ⸱ ", battery(&sys), ram(&sys), cpu(&sys)).to_string();
         {
             let mut df = conc.lock.lock().unwrap();
             if df.systemstat != new_stat {
@@ -127,7 +148,7 @@ fn sensors_thread(conc: Arc<Concurrency>) {
     let sensors = Sensors::new();
     if let Some(temp_sensor) = find_cpu_temp(&sensors) {
         loop {
-            let new_temp = format!("\u{1F321} {}", temp_sensor.get_value().unwrap());
+            let new_temp = format!("\u{1F321} {} ⸱ ", temp_sensor.get_value().unwrap());
             {
                 let mut df = conc.lock.lock().unwrap();
                 if df.temp != new_temp {
@@ -137,6 +158,72 @@ fn sensors_thread(conc: Arc<Concurrency>) {
             }
             thread::sleep(POLL_TIME);
         }
+    }
+}
+
+fn get_wireless_interfaces() -> HashSet<String> {
+    let procfile = File::open("/proc/net/wireless").unwrap();
+    let reader = BufReader::new(procfile);
+    let mut wifs = HashSet::new();
+    let wifre = Regex::new(r"^(\w+):").unwrap();
+    for line in reader.lines() {
+        if let Some(captures) = wifre.captures(&line.unwrap()) {
+            wifs.insert(captures.get(1).unwrap().as_str().to_string());
+        }
+    }
+    wifs
+}
+
+fn get_current_interface() -> Result<String> {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"dev (\w+) ").unwrap();
+    }
+    let output = Command::new("ip")
+        .arg("route")
+        .arg("get")
+        .arg("8.8.8.8")
+        .output()
+        .chain_err(|| "subprocess")?;
+    let output_string = String::from_utf8(output.stdout).chain_err(|| "from_utf8")?;
+    if let Some(captures) = RE.captures(&output_string) {
+        return Ok(captures.get(1).unwrap().as_str().to_string());
+    }
+    bail!("No current interface.")
+}
+
+fn network_thread(conc: Arc<Concurrency>) {
+    let wireless = "📡 ⸱ ";
+    let wired = "⇅ ⸱ ";
+    let wifs = get_wireless_interfaces();
+    let monitor = Command::new("ip")
+        .arg("monitor")
+        .arg("link")
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    let mut stdout = monitor.stdout.unwrap();
+    let mut buffer = [0; 1024];
+
+    loop {
+        let new_symbol = match get_current_interface() {
+            Ok(interface) => {
+                if wifs.contains(&interface) {
+                    wireless
+                } else {
+                    wired
+                }
+            }
+            Err(_) => "",
+        }.to_string();
+        {
+            let mut df = conc.lock.lock().unwrap();
+            if df.net != new_symbol {
+                df.net = new_symbol;
+                conc.condition.notify_one();
+            }
+        }
+        stdout.read(&mut buffer).unwrap();
     }
 }
 
@@ -172,6 +259,7 @@ fn main() {
             time: String::new(),
             systemstat: String::new(),
             temp: String::new(),
+            net: String::new(),
         }),
         condition: Condvar::new(),
     });
@@ -189,6 +277,11 @@ fn main() {
     {
         let conc2 = Arc::clone(&conc);
         thread::spawn(move || sensors_thread(conc2));
+    }
+
+    {
+        let conc2 = Arc::clone(&conc);
+        thread::spawn(move || network_thread(conc2));
     }
 
     display_thread(conc);
